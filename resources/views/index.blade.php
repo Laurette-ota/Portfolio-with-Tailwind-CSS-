5a9ede5f6519fc88b93a3d4ed1c76a9d5774ced2
Laurette <borisiakokouvi@gmail.com>
2025-02-17 11:06:13 +0100

    Ajout de la configuration Tailwind CSS, création du fichier de style principal et mise en place de la structure de la vue index.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/app.css" rel="stylesheet">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+</body>
+
+</html>
